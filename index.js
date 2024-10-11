7a307d831ebc92cab38771550281a7a27ae5625b
Newaldy21 <63190549+Newaldy21@users.noreply.github.com>
2024-10-11 15:18:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const { unwrap } = require('./src/module/wrap/unwrap');
 const BN = require('bn.js');
 
 function randomGasPrice(web3Instance) {
-    const minGwei = new BN(web3Instance.utils.toWei('0.05', 'gwei'));
-    const maxGwei = new BN(web3Instance.utils.toWei('0.06', 'gwei'));
+    const minGwei = new BN(web3Instance.utils.toWei('0.19', 'gwei'));
+    const maxGwei = new BN(web3Instance.utils.toWei('0.192', 'gwei'));
     const randomGwei = minGwei.add(new BN(Math.floor(Math.random() * (maxGwei.sub(minGwei).toNumber()))));
     return randomGwei;
 }
